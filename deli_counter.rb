4e9d8f2cba6f755eac2807c8ce49873b436f9fd8
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 03:02:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 def line(x)
-  line_array = []
+  katz_deli = []
   if x.length == 0 
     puts "The line is currently empty."
   else 
     x.each_with_index do |place, index| 
-    line_array.push("#{index + 1}. #{place}")
+    katz_deli.push("#{index + 1}. #{place}")
   end
-  puts "The line is currently: #{line_array.join(" ")}"
+  puts "The line is currently: #{katz_deli.join(" ")}"
 end
 end
 
-def take_a_number(deli, name)
+
   
   
