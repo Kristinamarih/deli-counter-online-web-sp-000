f135a32ff565b474b8be96a88dc7d8ea0905212a
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 01:52:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
 def line(katz_deli)
-  katz_deli.each{|place| puts "The line is currently: #{|place| + 1}. #{place}."
+  katz_deli.each{|place| puts "The line is currently: #{place + 1}. #{place}."
 end
   
