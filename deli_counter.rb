f7ce094d5986db46c7b1cb9bacb5a19783e961b2
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 02:15:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line(katz_deli)
   katz_deli.each_with_index do |place, index| 
-    puts "The line is currently: #{index + 1}. #{place}."
+    puts "The line is currently: #{index + 1}. #{place}"
   end
 end
   
