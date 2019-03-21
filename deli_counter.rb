b5eb8a84d29ef917f6661b1573acd314e5dc29cb
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-21 02:01:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,8 @@
 def line(array)
   array.each do |place| 
+    if array == [ ]
+      puts "The line is currently empty"
+    else 
     return "The line is currently: #{place + 1}. #{place}."
 end
 end
